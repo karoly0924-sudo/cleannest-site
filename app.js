c311af710e9dcad67ef7e6f861377974ad40dfe8
karoly0924-sudo <karoly0924@gmail.com>
2026-01-27 13:39:21 +0100

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-document.getElementById("bookBtn")?.addEventListener("click", () => {
-  alert("Foglalás hamarosan!");
-});
+// FILE: app.js (FULL REPLACE)
+(function () {
+  "use strict";
+
+  var reviewBtn = document.getElementById("reviewBtn");
+  if (reviewBtn) {
+    reviewBtn.addEventListener("click", function () {
+      var q = encodeURIComponent("CleanNest Hausservice e.U. Bewertung");
+      window.open("https://www.google.com/search?q=" + q, "_blank", "noopener,noreferrer");
+    });
+  }
+
+  var bookBtn = document.getElementById("bookBtn");
+  if (bookBtn) {
+    bookBtn.addEventListener("click", function () {
+      alert("Buchung folgt in Kürze.");
+    });
+  }
+})();
